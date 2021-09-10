df9b695c4b2bcd9ab27a73aaa7a9fadd83833a51
David-Nuzzo <DavidNuzzo1@gmail.com>
2021-09-10 23:12:45 +0100

    Started to add the table functionality.

--- app/src/main/java/com/example/wintracker/MainActivity.java
+++ app/src/main/java/com/example/wintracker/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.wintracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    int playerAmount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,8 +34,38 @@ public class MainActivity extends AppCompatActivity
         findViewById(R.id.PlayerNameBox).setVisibility(View.INVISIBLE);
         findViewById(R.id.AddPlayerButton).setVisibility(View.INVISIBLE);
 
-        // Remove Name
+        // Add a new row to the table with the players data.
+
+        TableLayout table = findViewById(R.id.Table);
+        TableRow tableRow = new TableRow(this);
+        tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        // Fill Position column.
+        TextView pos = new TextView(this);
+        pos.setText(playerAmount);
+        pos.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        pos.setGravity(Gravity.CENTER);
+        pos.setLayoutParams(new TableRow.LayoutParams(1));
+        tableRow.addView(pos);
+        // Fill Name column.
+        TextView name = new TextView(this);
+        name.setText("name2");
+        name.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        name.setGravity(Gravity.CENTER);
+        name.setLayoutParams(new TableRow.LayoutParams(2));
+        tableRow.addView(name);
+        // Fill Wins column
+        TextView wins = new TextView(this);
+        wins.setText("420");
+        wins.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        wins.setGravity(Gravity.CENTER);
+        wins.setLayoutParams(new TableRow.LayoutParams(2));
+        tableRow.addView(wins);
+        // Add row to table.
+        table.addView(tableRow, new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+
+        // Remove Name and and 1 to playerAmount.
         EditText PlayerNameBox = findViewById(R.id.PlayerNameBox);
         PlayerNameBox.setText("");
+        playerAmount ++;
     }
 }
