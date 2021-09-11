4282935078609344182c5c09d8aff1c4f91c294d
David-Nuzzo <DavidNuzzo1@gmail.com>
2021-09-12 00:16:22 +0100

    Selected row is now updated correctly on the plus button. Selected row is now coloured a light green for increased accessibility, Wins are now added on plus button press.

--- app/src/main/java/com/example/wintracker/MainActivity.java
+++ app/src/main/java/com/example/wintracker/MainActivity.java
@@ -3,13 +3,11 @@ package com.example.wintracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -58,8 +56,8 @@ public class MainActivity extends AppCompatActivity
 
         // Fill Name column.
         TextView name = new TextView(this);
-        EditText nameBox = (EditText) findViewById(R.id.PlayerNameBox);
-        String newPlayerName = (String) nameBox.getText().toString();
+        EditText nameBox = findViewById(R.id.PlayerNameBox);
+        String newPlayerName = nameBox.getText().toString();
         name.setText(newPlayerName);
         name.setGravity(Gravity.CENTER);
         name.setLayoutParams(new TableRow.LayoutParams(2));
@@ -99,52 +97,71 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                // Make all the rows and columns of the table have a white background.
-                // This loops through all table rows.
-                int counter = 4;
-                for(int i = 0; i < table.getChildCount(); i++)
-                {
-                    TableRow row = (TableRow) table.getChildAt(i);
-                    // This will iterate through the current rows columns. (Thus all rows columns when used with the above loop.)
-                    for(int j = 0; j < row.getChildCount(); j++)
-                    {
-                        // Leave the header row untouched.
-                        if(counter >= 1)
-                        {
-                            TextView element = (TextView) row.getChildAt(j);
-                            counter--;
-                        }
-                        else
-                        {
-                            TextView element = (TextView) row.getChildAt(j);
-                            element.setBackgroundColor(Color.WHITE);
-                        }
-                    }
-                }
-                counter = 4;
+                AllTableElementsWhiteBg(table);
 
-                // Highlighted the selected rows elements.
-                // Update the selectedPlayerPos.
+                // Highlighted the selected row blue & update the selectedPlayerPos.
                 selectedPlayerPos = Integer.parseInt(pos.getText().toString());
-                pos.setBackgroundColor(Color.BLUE);
-                name.setBackgroundColor(Color.BLUE);
-                wins.setBackgroundColor(Color.BLUE);
-                addWinBtn.setBackgroundColor(Color.BLUE);
+                pos.setBackgroundResource(R.color.light_green);
+                name.setBackgroundResource(R.color.light_green);
+                wins.setBackgroundResource(R.color.light_green);
+                addWinBtn.setBackgroundResource(R.color.light_green);
             }
         });
 
         // Set onClick listener for the plus button in each row.
         addWinBtn.setOnClickListener(new View.OnClickListener()
-         {
-             @Override
-             public void onClick(View view) {
+        {
+            @Override
+            public void onClick(View view)
+            {
+                AllTableElementsWhiteBg(table);
+                // Highlighted the selected row blue & update the selectedPlayerPos.
+                selectedPlayerPos = Integer.parseInt(pos.getText().toString());
+                pos.setBackgroundResource(R.color.light_green);
+                name.setBackgroundResource(R.color.light_green);
+                wins.setBackgroundResource(R.color.light_green);
+                addWinBtn.setBackgroundResource(R.color.light_green);
 
-             }
-         });
+
+                int currentWins = Integer.parseInt(wins.getText().toString());
+                wins.setText(Integer.toString(currentWins + 1));
+            }
+        });
 
         // Remove Name and and 1 to playerAmount.
         EditText PlayerNameBox = findViewById(R.id.PlayerNameBox);
         PlayerNameBox.setText("");
-        playerAmount ++;
+        playerAmount++;
     }
+
+    // Methods
+
+    public void AllTableElementsWhiteBg(TableLayout table)
+    {
+        // Make all the rows and columns of the table have a white background.
+        // This loops through all table rows.
+        int counter = 4;
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            // This will iterate through the current rows columns. (Thus all rows columns when used with the above loop.)
+            for (int j = 0; j < row.getChildCount(); j++)
+            {
+                // Leave the header row untouched.
+                if (counter >= 1)
+                {
+                    //TextView element = (TextView) row.getChildAt(j);
+                    counter--;
+                }
+                else
+                {
+                    TextView element = (TextView) row.getChildAt(j);
+                    element.setBackgroundColor(Color.WHITE);
+                }
+            }
+        }
+        counter = 4;
+    }
+
 }
+
+
